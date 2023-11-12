eaac1f8616fcc10605432cbcf5e699d216f58bfe
Bennett <bwm7637@g.rit.edu>
2023-11-12 18:54:27 -0500

    test_IMDN_AS.py can now perform both IMDN and IMND_AS testing

--- test_IMDN_AS.py
+++ test_IMDN_AS.py
@@ -17,8 +17,12 @@ parser.add_argument("--checkpoint", type=str, default='checkpoints/IMDN_AS.pth',
                     help='checkpoint folder to use')
 parser.add_argument('--cuda', action='store_true', default=True,
                     help='use cuda')
+parser.add_argument("--upscale_factor", type=int, default=2,
+                    help='upscaling factor') # Only applies to IMDN
 parser.add_argument("--is_y", action='store_true', default=False,
                     help='evaluate on y channel, if False evaluate on RGB channels')
+parser.add_argument("--use_as", type=bool, default=True,
+                    help='use IMDN_AS')
 opt = parser.parse_args()
 
 print(opt)
@@ -60,18 +64,24 @@ def crop_forward(x, model, shave=32):
     return output
 
 
+def parse_img_name(imname, extension):
+    result = imname.split('/')[-1]
+    if not opt.use_as:
+        result = result.split('.')[0] + 'x' + str(opt.upscale_factor) + extension
+    return result
+
 filepath = opt.test_hr_folder
 if filepath.split('/')[-2] == 'Set5' or filepath.split('/')[-2] == 'Set14':
     ext = '.bmp'
 else:
     ext = '.png'
-
+    
 filelist = utils.get_list(filepath, ext=ext)
 psnr_list = np.zeros(len(filelist))
 ssim_list = np.zeros(len(filelist))
 time_list = np.zeros(len(filelist))
 
-model = architecture.IMDN_AS()
+model = architecture.IMDN_AS() if opt.use_as else architecture.IMDN(upscale=opt.upscale_factor) 
 model_dict = utils.load_state_dict(opt.checkpoint)
 model.load_state_dict(model_dict, strict=True)
 
@@ -79,8 +89,13 @@ start = torch.cuda.Event(enable_timing=True)
 end = torch.cuda.Event(enable_timing=True)
 i = 0
 for imname in filelist:
-    im_gt = cv2.imread(imname)[:, :, [2, 1, 0]]
-    im_l = cv2.imread(opt.test_lr_folder + imname.split('/')[-1])[:, :, [2, 1, 0]]
+    if opt.use_as:
+        im_gt = cv2.imread(imname)[:, :, [2, 1, 0]]
+        im_l = cv2.imread(opt.test_lr_folder + parse_img_name(imname, ext))[:, :, [2, 1, 0]]
+    else:
+        im_gt = cv2.imread(imname, cv2.IMREAD_COLOR)[:, :, [2, 1, 0]]  # BGR to RGB
+        im_gt = utils.modcrop(im_gt, opt.upscale_factor)
+        im_l = cv2.imread(opt.test_lr_folder + parse_img_name(imname, ext), cv2.IMREAD_COLOR)[:, :, [2, 1, 0]]  # BGR to RGB
     if len(im_gt.shape) < 3:
         im_gt = im_gt[..., np.newaxis]
         im_gt = np.concatenate([im_gt] * 3, 2)
@@ -98,7 +113,7 @@ for imname in filelist:
     _, _, h, w = im_input.size()
     with torch.no_grad():
 
-        if h % 4 == 0 and w % 4 == 0:
+        if (h % 4 == 0 and w % 4 == 0) or not opt.use_as:
             start.record()
             out = model(im_input)
             end.record()
@@ -111,7 +126,15 @@ for imname in filelist:
             torch.cuda.synchronize()
             time_list[i] = start.elapsed_time(end)  # milliseconds
 
-    sr_img = utils.tensor2np(out.detach()[0])
+    out_img = utils.tensor2np(out.detach()[0])
+
+    if opt.use_as:
+        sr_img = utils.tensor2np(out.detach()[0])
+    else:   
+        crop_size = opt.upscale_factor
+        sr_img = utils.shave(out_img, crop_size)
+        im_gt = utils.shave(im_gt, crop_size)
+
     if opt.is_y is True:
         im_label = utils.quantize(sc.rgb2ycbcr(im_gt)[:, :, 0])
         im_pre = utils.quantize(sc.rgb2ycbcr(sr_img)[:, :, 0])
@@ -122,12 +145,12 @@ for imname in filelist:
     ssim_list[i] = utils.compute_ssim(im_pre, im_label)
 
     output_folder = os.path.join(opt.output_folder,
-                                 imname.split('/')[-1])
+                                 parse_img_name(imname, '.png'))
 
     if not os.path.exists(opt.output_folder):
         os.makedirs(opt.output_folder)
 
-    cv2.imwrite(output_folder, sr_img[:, :, [2, 1, 0]])
+    cv2.imwrite(output_folder, out_img[:, :, [2, 1, 0]])
     i += 1
 
 print("Mean PSNR: {}, SSIM: {}, Time: {} ms".format(np.mean(psnr_list), np.mean(ssim_list), np.mean(time_list)))
